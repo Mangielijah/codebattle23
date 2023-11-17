23e97a8852bf6e0388976b096b578ca9e55746f0
UltraTakow <alberttakow000@gmail.com>
2023-11-17 12:36:19 +0100

    new file:   lib/presentation/nav.dart 	new file:   lib/presentation/screens/CitizenSignup.dart 	modified:   lib/presentation/screens/home_screen.dart 	new file:   lib/presentation/screens/profile.dart 	new file:   lib/presentation/screens/request.dart 	new file:   lib/presentation/screens/user_type.dart

--- lib/presentation/components.dart
+++ lib/presentation/components.dart
@@ -1,4 +1,5 @@
 // import 'package:codebattle23/constants.dart';
+import 'package:codebattle23/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
